869dd30bea8129612a2257c8e3f16c75901abb25
Tianming Ren <1974645454@qq.com>
2026-02-01 23:41:47 +0900

    modified:   .gitignore         modified:   sql_app/models.py         modified:   sql_app/schemas.py

--- sql_app/models.py
+++ sql_app/models.py
@@ -10,13 +10,21 @@ class ConferenceRoom(Base):
     __tablename__ = 'conferencerooms'
     conferenceroom_id = Column(Integer, primary_key = True,index = True)
     conferenceroom_name = Column(String, unique = True, index = True)
-    capacity = Column(Integer, primary_key = True,index = True)
+    conferenceroom_capacity = Column(Integer, nullable=False)
 
 class Booking(Base):
     __tablename__ = 'bookings'
     booking_id = Column(Integer, primary_key = True,index = True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), ondelet = 'SET NULL', nullable = False)
-    conferenceroom_id = Column(Integer, ForeignKey('conferencerooms.conferenceroom_id'), ondelete = 'SET NULL', nullabl = False)
-    booked_capacity = Column(Integer)
+    user_id = Column(
+        Integer, 
+        ForeignKey('users.user_id', ondelete = 'SET NULL'), 
+        nullable = True
+        )
+    conferenceroom_id = Column(
+        Integer, 
+        ForeignKey('conferencerooms.conferenceroom_id', ondelete = 'SET NULL'), 
+        nullable = True
+        )
+    booking_capacity = Column(Integer, nullable = False)
     start_datetime = Column(DateTime, nullable = False)
     end_datetime = Column(DateTime, nullable = False)
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel, Field
 class Booking(BaseModel):
     booking_id: int
     user_id: int
-    conferenceroom_id: int 
-    booked_capacity: int
+    conferenceroom_id: int
+    booking_capacity: int
     start_datetime: datetime.datetime
     end_datetime: datetime.datetime
     
@@ -16,7 +16,13 @@ class User(BaseModel):
     user_id: int
     user_name: str = Field(max_length=64)
 
+    class Config:
+        orm_mode = True
+
 class ConferenceRoom(BaseModel):
     conferenceroom_id: int
     conferenceroom_name: str = Field(max_length=32)
-    capacity: int
+    conferenceroom_capacity: int
+
+    class Config:
+        orm_mode = True
