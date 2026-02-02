707f5fb23833d2ebaa5470c68f6904e9dae813af
Tianming Ren <1974645454@qq.com>
2026-02-03 00:31:09 +0900

    add fastapi post/get method on main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
-from . database import SessionLocal, engine
+from .database import SessionLocal, engine
 
 models.Base.metadata.create.all(bind=engine)
 
@@ -11,7 +11,6 @@ app = FastAPI()
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
@@ -24,29 +23,30 @@ async def index():
 """
 
 # Read
-@app.get("/users", response_model=List[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db:Session = Depends(get_db)):
+@app.get("/users", response_model = List[schemas.User])
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
-@app.get("/conferencerooms")
-async def conferencerooms(conferencerooms: ConferenceRoom):
-    return {"conferencerooms": conferencerooms}
+@app.get("/conferencerooms", response_model = List[schemas.ConferenceRoom])
+async def read_conferencerooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    conferencerooms = crud.get_conferencerooms(db, skip=skip, limit=limit)
+    return conferencerooms
 
-@app.get("/bookings")
-async def bookings(bookings: Booking):
-    return {"bookings": bookings}
+@app.get("/bookings", response_model = List[schemas.Booking])
+async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    bookings = crud.get_bookings(db, skip=skip, limit=limit)
+    return bookings
 
 # Create
-@app.post("/users")
-async def users(users: User):
-    return {"users": users}
+@app.post("/users",response_model=schemas.User)
+async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    return crud,create_user(db = db, user = user)
 
-@app.post("/conferencerooms")
-async def conferencerooms(conferencerooms: ConferenceRoom):
-    return {"conferencerooms": conferencerooms}
+@app.post("/conferencerooms",response_model=schemas.ConferenceRoom)
+async def create_conferenceroom(conferenceroom: schemas.ConferenceRoom, db: Session = Depends(get_db)):
+    return crud,create_conferenceroom(db = db, conferenceroom = conferenceroom)
 
-@app.post("/bookings")
-async def bookings(bookings: Booking):
-    return {"bookings": bookings}
-
+@app.post("/bookings",response_model=schemas.Booking)
+async def create_booking(booking: schemas.Booking, db: Session = Depends(get_db)):
+    return crud,create_booking(db = db, booking = booking)
