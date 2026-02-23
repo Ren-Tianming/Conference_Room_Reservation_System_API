79f36a14bdb35430374faeac9ead80b53b0fc634
Tianming Ren <1974645454@qq.com>
2026-02-24 00:36:43 +0900

    fixed database.py security

--- sql_app/database.py
+++ sql_app/database.py
@@ -12,15 +12,15 @@ ASYNC_DATABASE_URL = os.getenv("DATABASE_URL")
 
 async_engine = create_async_engine(
     ASYNC_DATABASE_URL,
-    echo=True,  
-    pool_size=10, 
+    echo=True,
+    pool_size=10,
     max_overflow=20
 )
 
 class Base(DeclarativeBase):
     create_time: Mapped[datetime] = mapped_column(
         DateTime,
-        server_default=now(), 
+        server_default=now(),
         comment="作成時間"
     )
     update_time: Mapped[datetime] = mapped_column(
@@ -31,15 +31,12 @@ class Base(DeclarativeBase):
     )
 
 AsyncSessionLocal = async_sessionmaker(
-    bind=async_engine, 
-    class_=AsyncSession, 
+    bind=async_engine,
+    class_=AsyncSession,
     expire_on_commit=False
 )
 
 async def create_table():
-    # print("--- テープルを作成中 ---")
-    # print(f"DEBUG　作成中のテープル: {Base.metadata.tables.keys()}")
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    # print("--- テープル作成完了 ---")
 
